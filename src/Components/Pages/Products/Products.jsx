57f085585b18ecf8ec715368d1d89a83a4e60bfe
Punith Nandiraj <punithnandiraj@gmail.com>
2020-10-27 17:08:10 +0530

    Completed the react sample website

---
+++ src/Components/Pages/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import { homeObjFour } from './Data';
+
+
+function Products() {
+  return (
+    <div>
+    <HeroSection {...homeObjFour} />
+    </div>
+  );
+}
+
+export default Products;
